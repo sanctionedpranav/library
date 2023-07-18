ace9e13ca7f88083837be35e987001eac283d1ef
sanctionedpranav <pranav9728@gmail.com>
2023-07-18 13:19:41 +0530

    Create new Author done message left

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const Author = require('../modals/author')
 // all authors route
 router.get('/', (req, res) => {
     res.render('authors/index')
@@ -8,12 +8,43 @@ router.get('/', (req, res) => {
 
 // new author route
 router.get('/new', (req, res) => {
-    res.render('authors/new')
+    res.render('authors/new', { author: new Author() })
 })
 
 // create author route
-router.post('/', (req, res)=>{
-    res.send('Create')
-})
+// router.post('/', (req, res) => {
+//     const author = new Author({
+//         name: req.body.name
+//     })
+//     author.save((err, newAuthor) => {
+//         if (err) {
+//             res.render('authors/new', ({
+//                 author: author,
+//                 errorMessage: 'Error creating author'
+//             }))
+//         }
+//         else {
+//             // res.redirect(`authors/${newAuthor.id}`)
+//             res.redirect(`authors`)
+//         }
+//     })
+// })
+
+
+router.post('/', async (req, res) => {
+    try {
+        const author = new Author({
+            name: req.body.name
+        });
+        const newAuthor = await author.save();
+        // res.redirect(`authors/${newAuthor.id}`);
+        res.redirect('authors');
+    } catch (err) {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating author'
+        });
+    }
+});
 
 module.exports = router
