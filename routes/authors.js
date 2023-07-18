6cdfc287b6087a6380458e743f04d90b02684ef5
sanctionedpranav <pranav9728@gmail.com>
2023-07-18 21:52:20 +0530

    All authors, create author, and its routes

--- routes/authors.js
+++ routes/authors.js
@@ -2,8 +2,20 @@ const express = require('express')
 const router = express.Router()
 const Author = require('../modals/author')
 // all authors route
-router.get('/', (req, res) => {
-    res.render('authors/index')
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== "") {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions: req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
 })
 
 // new author route
@@ -12,34 +24,15 @@ router.get('/new', (req, res) => {
 })
 
 // create author route
-// router.post('/', (req, res) => {
-//     const author = new Author({
-//         name: req.body.name
-//     })
-//     author.save((err, newAuthor) => {
-//         if (err) {
-//             res.render('authors/new', ({
-//                 author: author,
-//                 errorMessage: 'Error creating author'
-//             }))
-//         }
-//         else {
-//             // res.redirect(`authors/${newAuthor.id}`)
-//             res.redirect(`authors`)
-//         }
-//     })
-// })
-
-
 router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    });
     try {
-        const author = new Author({
-            name: req.body.name
-        });
         const newAuthor = await author.save();
-        // res.redirect(`authors/${newAuthor.id}`);
+        // res.redirect(`authors/${newAuthor._id}`);
         res.redirect('authors');
-    } catch (err) {
+    } catch {
         res.render('authors/new', {
             author: author,
             errorMessage: 'Error creating author'
@@ -47,4 +40,4 @@ router.post('/', async (req, res) => {
     }
 });
 
-module.exports = router
+module.exports = router 
